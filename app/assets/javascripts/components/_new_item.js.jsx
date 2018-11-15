50c40c0fe31cf87641809d4ebb1b04dac7602662
garyCoffey <garycoffey12@gmail.com>
2018-11-15 15:53:23 -0600

    WIP

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -2,11 +2,19 @@ const NewPost = (props) => {
   let formFields = {}
 
   return (
+
     <form onSubmit={(e) => { props.handleFormSubmit(formFields.address.value, formFields.blog.value); e.target.reset(); }
     }>
-      <input ref={input => formFields.address = input} placeholder='Enter the address of the arcade' />
-      <input ref={input => formFields.blog = input} placeholder='Enter your blog' />
-      <button>Submit</button>
+      <div className="form-group">
+        <label>Your BloooahhhG</label>
+        <textarea ref={input => formFields.blog = input} placeholder='Enter your blog' type="string" className="form-control" />
+      </div>
+
+      <div className="form-group">
+        <label>Address</label>
+        <input ref={input => formFields.address = input} placeholder='Enter the address of the arcade' type="string" className="form-control" />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   )
 }
