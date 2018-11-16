e97451a6ce744bb8e0fdb8e459c0b9122eef9265
garyCoffey <garycoffey12@gmail.com>
2018-11-15 23:56:21 -0600

    WIP

--- app/assets/javascripts/components/_post.js.jsx
+++ app/assets/javascripts/components/_post.js.jsx
@@ -22,7 +22,7 @@ class Post extends React.Component {
 
   render() {
     let blog = this.state.editable ? <input type='text' ref={input => this.blog = input} defaultValue={this.props.post.blog} className="form-control" /> : <h3>{this.props.post.blog}</h3>
-    let address = this.state.editable ? <a href={this.props.showLink}><input type='text' ref={input => this.address = input} defaultValue={this.props.post.address} className="form-control" /></a> : <a href={this.props.showLink}><p>{this.props.post.address}</p></a>
+    let address = this.state.editable ? <input type='text' ref={input => this.address = input} defaultValue={this.props.post.address} className="form-control" /> : <a href={this.props.showLink}><p>{this.props.post.address}</p></a>
 
     return (
       <div>
